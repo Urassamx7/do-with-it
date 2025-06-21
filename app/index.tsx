9567d2ff472a4f05fbacbf3e01d3e9f31eaeba9c
Marvin Ernesto <marvin.lagas01234@gmail.com>
2025-06-21 10:01:38 +0200

    improve view image screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,8 @@
 import { useDeviceOrientation } from "@react-native-community/hooks";
-import { View } from "react-native";
-import Card from "./components/card";
-import image from "./assets/images/jacket.jpg";
-import ListingDetailsScreen from "./screens/listing-details-screen";
+import ViewImageScreen from "./screens/view-image-screen";
 
 export default function Index() {
   const orietantion = useDeviceOrientation();
 
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
