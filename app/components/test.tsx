c5d33cab1b390131715d5561c4249297226e5b70
Marvin Ernesto <marvin.lagas01234@gmail.com>
2025-06-25 10:43:05 +0200

    improve rafce

--- app/components/test.tsx
+++ app/components/test.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
 const Test = () => {
-return (
-  <View style={styles.container}></View>
-)
- }
-
- const styles = StyleSheet.create({
-container:{
+  return (
+    <View style={styles.container}></View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
 })
+
 export default Test
