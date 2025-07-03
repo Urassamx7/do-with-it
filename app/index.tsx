3becb51cedbd3f3dc1f5d4f0629c5a102f0f1c0a
Marvin Ernesto <marvin.lagas01234@gmail.com>
2025-07-03 19:50:36 +0200

    configure splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -5,18 +5,39 @@ import AuthNavigator from "./navigation/auth-navigator";
 import AuthContext, { UserProfile } from "./auth/auth-context";
 import authStorage from "./auth/storage";
 import { jwtDecode } from "jwt-decode";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Index() {
   const [user, setUser] = useState<UserProfile | null>(null);
+  const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
-  };
   useEffect(() => {
-    restoreToken;
+    const prepare = async () => {
+      try {
+        const token = await authStorage.getToken();
+        if (token) {
+          try {
+            const decodedUser = jwtDecode<UserProfile>(token);
+            setUser(decodedUser);
+          } catch (err) {
+            console.warn("Token inválido", err);
+            await authStorage.removeToken();
+          }
+        }
+      } catch (error) {
+        console.error("Erro ao restaurar token:", error);
+      } finally {
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    prepare();
   }, []);
+
+  if (!isReady) return null;
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
