a681fadc64ef1d410ed1d5603342a3cba4d02205
Marvin Ernesto <marvin.lagas01234@gmail.com>
2025-07-03 15:42:12 +0200

    request with cache

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,7 +1,31 @@
-import { create } from 'apisauce'
+import { ApiOkResponse, ApiResponse, create } from 'apisauce'
+import cache from '../utils/cache'
+import { AxiosRequestConfig } from 'axios';
 
 const apiClient = create({
     baseURL: 'http://172.20.10.5:3333/api'
 })
+
+
+const get = apiClient.get
+
+apiClient.get = async function <T, U = T>(
+    url: string,
+    params?: {} | undefined,
+    axiosConfig?: AxiosRequestConfig
+): Promise<ApiResponse<T, U>> {
+    const response = await get<T, U>(url, params, axiosConfig)
+
+    if (response.ok) {
+        cache.store(url, response.data);
+        return response;
+    }
+
+    const data = await cache.get(url);
+    return data
+        ? { ok: true, data } as ApiOkResponse<T>
+        : response;
+};
+
 
 export default apiClient
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,5 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import AppNavigator from "./navigation/app-navigator";
 
 export default function Index() {
-  const {} = useNetInfo();
-  const demo = async () => {
-    try {
-      await AsyncStorage.setItem(
-        "person",
-        JSON.stringify({
-          Name: "Marvin",
-          id: 1,
-          Age: 14,
-        })
-      );
-
-      const value = await AsyncStorage.getItem("person");
-      const person = JSON.parse(value!);
-      console.log(person);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  demo();
-  return null;
+  return <AppNavigator />;
 }
